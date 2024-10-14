2a1009be0e5533adff639478f81dbea7cae2c010
N.M.H <36321687+NakamotMatosH@users.noreply.github.com>
2024-10-14 19:59:52 +0900

    Update compute.py

--- loadstockdata/compute.py
+++ loadstockdata/compute.py
@@ -3,7 +3,9 @@ import pandas as pd
 import re
 import datetime
 
-def compute_rsi(dataframe, period=14):
+
+
+def compute_rsi(dataframe, price_column='closePrice', period=14):
     """
     RSI를 계산하는 함수입니다.
     Calculates the RSI.
@@ -11,6 +13,8 @@ def compute_rsi(dataframe, period=14):
     Parameters:
         dataframe (pandas.DataFrame): 가격 데이터프레임입니다.
                                       Price DataFrame.
+        price_column (str): 가격 열 이름입니다.
+                            The column name for price data.
         period (int): RSI 계산 기간입니다.
                       Period for RSI calculation.
 
@@ -18,12 +22,10 @@ def compute_rsi(dataframe, period=14):
         pandas.Series: RSI 값이 담긴 시리즈입니다.
                        Series containing RSI values.
     """
-    delta = dataframe['closePrice'].diff()
+    delta = dataframe[price_column].diff()
     gain = delta.clip(lower=0)
     loss = -delta.clip(upper=0)
 
-    # 지수 이동 평균(EMA) 사용으로 RSI 계산 최적화
-    # Use Exponential Moving Average (EMA) for smoother RSI
     avg_gain = gain.ewm(com=period - 1, min_periods=period).mean()
     avg_loss = loss.ewm(com=period - 1, min_periods=period).mean()
 
@@ -31,7 +33,8 @@ def compute_rsi(dataframe, period=14):
     rsi = 100 - (100 / (1 + rs))
     return rsi
 
-def compute_moving_average(dataframe, window_size):
+
+def compute_moving_average(dataframe, price_column='closePrice', window_size=30):
     """
     N일 이동평균선을 계산하는 함수입니다.
     Calculates the N-day Moving Average.
@@ -39,6 +42,8 @@ def compute_moving_average(dataframe, window_size):
     Parameters:
         dataframe (pandas.DataFrame): 가격 데이터프레임입니다.
                                       Price DataFrame.
+        price_column (str): 가격 열 이름입니다.
+                            The column name for price data.
         window_size (int): 이동평균 기간(n일)입니다.
                            The window size for moving average.
 
@@ -46,7 +51,6 @@ def compute_moving_average(dataframe, window_size):
         pandas.Series: 이동평균 값이 담긴 시리즈입니다.
                        Series containing moving average values.
     """
-    # 단순 이동평균(SMA) 계산
-    # Calculate Simple Moving Average (SMA)
-    moving_avg = dataframe['closePrice'].rolling(window=window_size, min_periods=1).mean()
+    moving_avg = dataframe[price_column].rolling(window=window_size, min_periods=1).mean()
     return moving_avg
+
