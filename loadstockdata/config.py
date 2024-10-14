0bd2da59ebfb7bcad8c0026e138177efd21bb513
N.M.H <36321687+NakamotMatosH@users.noreply.github.com>
2024-10-14 19:50:27 +0900

    Create config.py

---
+++ loadstockdata/config.py
@@ -0,0 +1,19 @@
+# config.py: API 요청에 필요한 설정 정보
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+    "authority": "api.nasdaq.com",
+    "accept": "application/json, text/plain, */*",
+    "accept-encoding": "gzip, deflate, br, zstd",
+    "accept-language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7,pl;q=0.6",
+    "dnt": "1",
+    "origin": "https://www.nasdaq.com",
+    "priority": "u=1, i",
+    "referer": "https://www.nasdaq.com/",
+    "sec-ch-ua": "\"Google Chrome\";v=\"125\", \"Chromium\";v=\"125\", \"Not.A/Brand\";v=\"24\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site"
+}
